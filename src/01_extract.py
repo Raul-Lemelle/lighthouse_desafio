769c382a10f5b7cc6a507c4a6c114303f0a7c536
${GITHUB_USER} <${GITHUB_MAIL}>
2024-07-07 04:51:25 -0300

    add first files

---
+++ src/01_extract.py
@@ -0,0 +1,56 @@
+import zipfile
+import os
+from dotenv import load_dotenv
+
+def extract_zip_file(zip_path, extract_path):
+    """
+    Extrai um arquivo zip para um diretório específico se os arquivos ainda não foram extraídos.
+
+    Args:
+    - zip_path (str): Caminho para o arquivo zip a ser extraído.
+    - extract_path (str): Caminho para o diretório onde o arquivo zip será extraído.
+    """
+    # Verificar se o arquivo zip existe
+    if not os.path.exists(zip_path):
+        raise FileNotFoundError(f"O arquivo {zip_path} não foi encontrado. Verifique o caminho e a existência do arquivo.")
+
+    # Verificar se o diretório de extração existe, caso contrário, criá-lo
+    if not os.path.exists(extract_path):
+        os.makedirs(extract_path)
+
+    # Verificar quais arquivos já existem no diretório de extração
+    existing_files = set(os.listdir(extract_path))
+
+    # Tentar abrir o arquivo zip e extrair apenas arquivos que não foram extraídos ainda
+    try:
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_files = set(zip_ref.namelist())
+            files_to_extract = zip_files - existing_files
+            
+            if not files_to_extract:
+                print(f"Todos os arquivos do zip já foram extraídos em {extract_path}.")
+            else:
+                for file in files_to_extract:
+                    zip_ref.extract(file, extract_path)
+                print(f"Arquivos extraídos com sucesso em {extract_path}: {files_to_extract}")
+    except zipfile.BadZipFile:
+        raise zipfile.BadZipFile(f"O arquivo {zip_path} não é um arquivo zip válido.")
+    except Exception as e:
+        raise Exception(f"Ocorreu um erro ao extrair o arquivo: {e}")
+
+if __name__ == "__main__":
+    # Carregar variáveis de ambiente do arquivo .env
+    load_dotenv()
+
+    # Obter caminhos do arquivo zip e do diretório de extração das variáveis de ambiente
+    zip_path = os.getenv('ZIP_PATH')
+    extract_path = os.getenv('EXTRACT_PATH')
+
+    # Verificar se as variáveis de ambiente foram carregadas corretamente
+    if not zip_path:
+        raise ValueError("A variável de ambiente ZIP_PATH não está definida.")
+    if not extract_path:
+        raise ValueError("A variável de ambiente EXTRACT_PATH não está definida.")
+
+    # Chamar a função para extrair o arquivo zip
+    extract_zip_file(zip_path, extract_path)
---
+++ src/02_load_data.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+
+def load_and_display_data(directory):
+    # Dicionário para armazenar os DataFrames
+    dataframes = {}
+
+    # Percorrer todos os arquivos na pasta especificada
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            # Caminho completo do arquivo
+            file_path = os.path.join(directory, filename)
+            # Nome do DataFrame com base no nome do arquivo
+            df_name = "DF_" + filename.split('.')[0].upper()
+            # Carregar o CSV em um DataFrame
+            dataframes[df_name] = pd.read_csv(file_path)
+            # Exibir as primeiras linhas do DataFrame
+            print(f"{filename}:")
+            print(dataframes[df_name].head(), "\n")
+
+    return dataframes
+
+if __name__ == "__main__":
+    
+    # Carregar variáveis de ambiente do arquivo .env
+    load_dotenv()
+
+    # Caminho para a pasta que contém os arquivos CSV
+    directory = os.getenv('DIRECTORY')
+    
+    # Chamada da função para visualizar os dataframes 
+    DATAFRAMES = load_and_display_data(directory)
